292e29dbfc84f9473d3d35d2abe5db7ed179ea03
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2020-08-15 15:30:56 +0300

    Updates

---
+++ precache-manifest.372353fe2aea8dedea15b8a1af7fcf62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bc8511e5bf4dd25ce46f1fe48120d18",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "ecfe0182104b9299b558",
+    "url": "/game-of-life/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "14913d64fe59c028e048",
+    "url": "/game-of-life/static/js/2.407701d2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-life/static/js/2.407701d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecfe0182104b9299b558",
+    "url": "/game-of-life/static/js/main.bde241c5.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
