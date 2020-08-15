1d7340bb2584d918cd78c12deab94c0db09c285b
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2020-08-15 14:17:39 +0300

    display interactive grid and start-stop button

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "./App.css";
 
+import produce from "immer";
+
 const numRows = 50;
 const numCols = 50;
 
@@ -15,7 +17,48 @@ const App = () => {
     return rows;
   });
 
-  return <div className="App"></div>;
+  const [isRunning, setIsRunning] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setIsRunning(!isRunning);
+        }}
+      >
+        {isRunning ? "stop" : "start"}
+      </button>
+      <div className="App">
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: `repeat(${numCols}, 20px)`,
+          }}
+        >
+          {grid.map((row, i) =>
+            row.map((col, j) => (
+              <div
+                key={`${i}-${j}`}
+                onClick={() => {
+                  setGrid(
+                    produce(grid, (draftGrid) => {
+                      draftGrid[i][j] = grid[i][j] ? 0 : 1;
+                    })
+                  );
+                }}
+                style={{
+                  width: 20,
+                  height: 20,
+                  backgroundColor: grid[i][j] ? "black" : undefined,
+                  border: "solid 1px black",
+                }}
+              />
+            ))
+          )}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
