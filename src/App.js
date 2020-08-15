c481aba06a4bf09c0f4434f6595f1153126df47c
ardaatahan <63500745+ardaatahan@users.noreply.github.com>
2020-08-15 13:18:15 +0300

    create grid

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const numRows = 50;
+const numCols = 50;
+
+const App = () => {
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+
+    for (let i = 0; i < numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+  });
+
+  return <div className="App"></div>;
+};
 
 export default App;
